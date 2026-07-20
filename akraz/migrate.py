7c54c34a3c425cf407563cf205985f2f892f2969
nilesh-greycube <nilesh@greycube.in>
2026-07-20 11:37:35 +0530

    fix: added MO in Quotation's connections

--- akraz/migrate.py
+++ akraz/migrate.py
@@ -3,6 +3,25 @@ from __future__ import unicode_literals
 import frappe
 from frappe.custom.doctype.custom_field.custom_field import create_custom_fields
 
+def update_dashboard_link_for_core_doctype(doctype,link_doctype,link_fieldname,group=None):
+    print(doctype,link_doctype,link_fieldname,group)
+    try:
+        d = frappe.get_doc("Customize Form")
+        if doctype:
+            d.doc_type = doctype
+        d.run_method("fetch_to_customize")
+        for link in d.get('links'):
+            if link.link_doctype==link_doctype and link.link_fieldname==link_fieldname:
+                # found so just return
+                return
+        d.append('links', dict(link_doctype=link_doctype, link_fieldname=link_fieldname,table_fieldname=None,group=group))
+        d.run_method("save_customization")
+        frappe.clear_cache()
+    except Exception:
+        frappe.log_error(frappe.get_traceback())
+
+update_dashboard_link_for_core_doctype(doctype='Quotation',link_doctype='Manufacturing Order AK',link_fieldname='quotation_reference',group="Manufacturing Order")
+
 
 def after_migrate():
     custom_fields = {
